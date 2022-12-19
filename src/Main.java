032f3cf2e0e89c83eefc42038aefa93fbc2050fb
ilia0312 <stashkov.ilia0312@yandex.ru>
2022-12-19 21:39:06 +0400

    Задачи 4-5

--- src/Main.java
+++ src/Main.java
@@ -17,6 +17,20 @@ public class Main {
         cat = cat - 1.6;
         System.out.println(cat);
         paper = paper - 7639;
-        System.out.println(paper);;
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
     }
 }
